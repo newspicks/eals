0da050fc6e47535832d25f2fdd1b6f1bf08408f5
Michihiro Nakamura <michihiro.nakamura@uzabase.com>
2021-07-14 04:10:56 +0900

    Add an e2e test for the serializer and deserializer

--- eals/serializer.py
+++ eals/serializer.py
@@ -7,14 +7,26 @@ import scipy.sparse as sps
 from .eals import ElementwiseAlternatingLeastSquares
 
 
+class NumpyArrayEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(NumpyArrayEncoder, self).default(obj)
+
+
 def serialize_json(file: str, model: ElementwiseAlternatingLeastSquares, compress: bool = True):
     model_dict = _serialize_json_lil(model)
     if compress:
         with gzip.open(file, "wb") as g:
-            g.write(json.dumps(model_dict).encode("utf-8"))
+            g.write(json.dumps(model_dict, cls=NumpyArrayEncoder).encode("utf-8"))
     else:
         with open(file, "w") as f:
-            json.dump(model_dict, f)
+            json.dump(model_dict, f, cls=NumpyArrayEncoder)
 
 
 def deserialize_json(file: str) -> ElementwiseAlternatingLeastSquares:
@@ -30,15 +42,17 @@ def deserialize_json(file: str) -> ElementwiseAlternatingLeastSquares:
 
 
 def _serialize_json_lil(model: ElementwiseAlternatingLeastSquares) -> dict:
-    model_dict = {"mode": "lil"}
+    model_dict = dict()
     # model initializer arguments
+    print(f"{model.factors=}")
     model_dict["factors"] = model.factors
+    print(f"{model_dict['factors']=}")
     model_dict["w0"] = model.w0
     model_dict["alpha"] = model.alpha
     model_dict["reg"] = model.reg
     model_dict["init_mean"] = model.init_mean
     model_dict["init_stdev"] = model.init_stdev
-    model_dict["dtype"] = model.dtype
+    # model_dict["dtype"] = model.dtype
     model_dict["max_iter"] = model.max_iter
     model_dict["max_iter_online"] = model.max_iter_online
     model_dict["random_state"] = model.random_state
@@ -53,6 +67,7 @@ def _serialize_json_lil(model: ElementwiseAlternatingLeastSquares) -> dict:
     model_dict["user_items"]["data"] = user_items.data
     model_dict["user_items"]["row"] = user_items.row
     model_dict["user_items"]["col"] = user_items.col
+    model_dict["user_items"]["shape"] = user_items.shape
     # TODO: WとWiの扱いをどうするか
     #   W: 現状では1固定なのでまあ無視でOK
     #   Wi: deserialize時にinit_data()で計算されるが、モデル保存時の値とは一致しない可能性あり（init_data()とupdate_model()が整合的でないので）
@@ -61,17 +76,17 @@ def _serialize_json_lil(model: ElementwiseAlternatingLeastSquares) -> dict:
 
 def _deserialize_json_lil(model_dict: dict) -> ElementwiseAlternatingLeastSquares:
     # model initializer arguments
-    factors = (model_dict.get("factors") or 64,)
-    w0 = (model_dict.get("w0") or 10,)
-    alpha = (model_dict.get("alpha") or 0.75,)
-    reg = (model_dict.get("reg") or 0.01,)
-    init_mean = (model_dict.get("init_mean") or 0,)
-    init_stdev = (model_dict.get("init_stdev") or 0.01,)
-    dtype = (model_dict.get("dtype") or np.float32,)
-    max_iter = (model_dict.get("max_iter") or 500,)
-    max_iter_online = (model_dict.get("max_iter_online") or 1,)
-    random_state = (model_dict.get("random_state") or None,)
-    show_loss = (model_dict.get("show_loss") or False,)
+    factors = (model_dict.get("factors") or 64)
+    w0 = (model_dict.get("w0") or 10)
+    alpha = (model_dict.get("alpha") or 0.75)
+    reg = (model_dict.get("reg") or 0.01)
+    init_mean = (model_dict.get("init_mean") or 0)
+    init_stdev = (model_dict.get("init_stdev") or 0.01)
+    # dtype = (model_dict.get("dtype") or np.float32,)
+    max_iter = (model_dict.get("max_iter") or 500)
+    max_iter_online = (model_dict.get("max_iter_online") or 1)
+    random_state = (model_dict.get("random_state") or None)
+    show_loss = (model_dict.get("show_loss") or False)
     # model parameters
     U = np.array(model_dict.get("U")) if model_dict.get("U") else None
     V = np.array(model_dict.get("V")) if model_dict.get("V") else None
@@ -82,11 +97,11 @@ def _deserialize_json_lil(model_dict: dict) -> ElementwiseAlternatingLeastSquare
         user_items_data = user_items_dict["data"]
         user_items_rows = user_items_dict["row"]
         user_items_cols = user_items_dict["col"]
+        user_items_shape = user_items_dict["shape"]
         # TODO: 行列のshapeも保存しておくべき？
-        user_count = max(user_items_rows)
-        item_count = max(user_items_cols)
         user_items = sps.csr_matrix(
-            (user_items_data, (user_items_rows, user_items_cols)), shape=(user_count, item_count)
+            (user_items_data, (user_items_rows, user_items_cols)),
+            shape=user_items_shape
         )
     else:
         user_items = sps.csr_matrix(([], ([], [])), shape=(0, 0))
@@ -98,7 +113,8 @@ def _deserialize_json_lil(model_dict: dict) -> ElementwiseAlternatingLeastSquare
         reg,
         init_mean,
         init_stdev,
-        dtype,
+        # TODO: Enable serialization of dtype
+        np.float32,
         max_iter,
         max_iter_online,
         random_state,
--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,3 +1,4 @@
+from typing import Union
 import pytest
 import numpy as np
 import scipy.sparse as sps
@@ -6,5 +7,38 @@ from eals.eals import ElementwiseAlternatingLeastSquares
 from eals.serializer import serialize_json, deserialize_json
 
 
-def teste_serializer():
-    pass
+def test_serialize_and_deserialize_without_compression(tmp_path):
+    # 3 users x 2 items
+    user_items = sps.csr_matrix([[1.0, 0.0], [1.0, 1.0], [0.0, 0.0]])
+    model = ElementwiseAlternatingLeastSquares(
+        factors=64,  # dimension of latent vectors
+        w0=10,  # overall weight of missing data
+        alpha=0.75,  # control parameter for significance level of popular items
+        reg=0.01,  # regularization parameter lambda
+        init_mean=0,  # mean of initial lantent vectors
+        init_stdev=0.01,  # stdev of initial lantent vectors
+        dtype=np.float32,
+        max_iter=1,
+        max_iter_online=1,
+        random_state=None,
+        show_loss=False,
+    )
+    model.fit(user_items)
+    file = (tmp_path / "model.json.gz").as_posix()
+    serialize_json(file, model)
+    model_actual = deserialize_json(file)
+    assert model.factors == model_actual.factors
+    assert model.w0 == model_actual.w0
+    assert model.alpha == model_actual.alpha
+    assert model.reg == model_actual.reg
+    assert model.init_mean == model_actual.init_mean
+    assert model.init_stdev == model_actual.init_stdev
+    assert model.dtype == model_actual.dtype
+    assert model.max_iter == model_actual.max_iter
+    assert model.max_iter_online == model_actual.max_iter_online
+    assert model.random_state == model_actual.random_state
+    assert model.show_loss == model_actual.show_loss
+    assert np.allclose(model.U, model_actual.U)
+    assert np.allclose(model.V, model_actual.V)
+    assert (model.user_items_lil.data == model_actual.user_items_lil.data).all()
+    assert (model.user_items_lil.rows == model_actual.user_items_lil.rows).all()
