fc03a119c95979684037393716f246e583faf5bf
Akira Kitauchi <kitauchi@gmail.com>
2021-09-08 22:10:41 +0900

    modify required versions of libraries

--- eals/__init__.py
+++ eals/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 from .eals import ElementwiseAlternatingLeastSquares, load_model
 
 __all__ = ["ElementwiseAlternatingLeastSquares", "load_model", "__version__"]
