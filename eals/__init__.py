ba66d17a6a06e863d91fc626b3bede8db2d20aed
Akira Kitauchi <kitauchi@gmail.com>
2021-11-25 17:32:44 +0900

    version 0.9.4

--- eals/__init__.py
+++ eals/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.9.3"
+__version__ = "0.9.4"
 from .eals import ElementwiseAlternatingLeastSquares, load_model
 
 __all__ = ["ElementwiseAlternatingLeastSquares", "load_model", "__version__"]
